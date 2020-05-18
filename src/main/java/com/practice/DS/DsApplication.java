405ed47445644732507f0506936e7180631c8245
Naman275 <54975652+Naman275@users.noreply.github.com>
2020-05-18 13:41:53 +0530

    initialize repo

---
+++ src/main/java/com/practice/DS/DsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.DS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/DS/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.DS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
