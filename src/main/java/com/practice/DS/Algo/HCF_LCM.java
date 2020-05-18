7d703a6e4601e9e549f83abd2c2012b8aef33421
Naman275 <54975652+Naman275@users.noreply.github.com>
2020-05-18 18:41:21 +0530

    checking method level validation

---
+++ src/main/java/com/practice/DS/Algo/HCF_LCM.java
@@ -0,0 +1,37 @@
+package com.practice.DS.Algo;
+import javax.validation.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.executable.ExecutableValidator;
+import javax.validation.groups.Default;
+import java.util.Set;
+class HCF{
+    public void check(@NotNull(message = "first name cannot be null") String ab, @NotNull  String cv){
+        System.out.println("hi baby there");
+    }
+}
+public class HCF_LCM {
+    private  static Validator validator;
+
+    public static void main(String [] args) throws NoSuchMethodException {
+        //ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+       // Validator validator = factory.getValidator();
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+
+        HCF ob=new HCF();
+        Class cl=ob.getClass();
+        ExecutableValidator executableValidator = validator.forExecutables();
+        Set<ConstraintViolation<Object>> violations = executableValidator.validateParameters(ob,cl.getMethod("check",String.class,String.class),
+        new Object[]{null,null},Default.class
+        );
+        ob.check("",null);
+    }
+
+    public static int findHCF(@NotNull int firstNo, @NotNull int secondNo){
+        return firstNo%secondNo==0?firstNo:findHCF(firstNo%secondNo,firstNo);
+    }
+    public static int findLCM(@NotNull  int firstNo,@NotNull  int secondNo){
+        return (firstNo*secondNo)/findHCF(firstNo,secondNo);
+    }
+}
