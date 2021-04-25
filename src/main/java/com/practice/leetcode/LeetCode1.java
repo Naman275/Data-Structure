aff4aba63fd7ef793f6172fe92fc496a72425729
Naman275 <54975652+Naman275@users.noreply.github.com>
2021-04-25 21:47:06 +0530

    master: Solved leetcode graph questions: https://leetcode.com/problems/k-similar-strings/ https://leetcode.com/problems/keys-and-rooms https://leetcode.com/problems/group-anagrams https://leetcode.com/problems/similar-string-groups

--- src/main/java/com/practice/leetcode/LeetCode1.java
+++ src/main/java/com/practice/leetcode/LeetCode1.java
@@ -1,41 +1,141 @@
 package com.practice.leetcode;
 
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 
 public class LeetCode1 {
-    int minCost=Integer.MAX_VALUE;
-    int time=0;
-    public static final String MOBILE_NO_REGEX="[0-9]{10}";
+    int minCost = Integer.MAX_VALUE;
+    int time = 0;
+    public static final String MOBILE_NO_REGEX = "[0-9]{10}";
 
-    public static void main(String []args){
+    Map<String, Integer> mapit = new HashMap<>();
 
-        Date d=new Date();
-        SimpleDateFormat dateFmt = new
-                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        System.out.println(dateFmt.format(new Date()));
-        if(true)return;
-        String ab="1234";
-        System.out.println(ab.matches(MOBILE_NO_REGEX));
-        ab="0123456789";
-        System.out.println(ab.matches(MOBILE_NO_REGEX));
-        ab="012345678f";
-        System.out.println(ab.matches(MOBILE_NO_REGEX));
-        ab="";
-        System.out.println(ab.matches(MOBILE_NO_REGEX));
-        ab="000000000ijk0000000000@";
-        System.out.println(ab.matches(MOBILE_NO_REGEX));
+    public static void main(String[] args) {
+        String ab = "cdsf";
+    }
+
+    //https://leetcode.com/problems/k-similar-strings/
+    public int kSimilarity(String s1, String s2) {
+        mapit = new HashMap<>();
+        return kSimilarity(s1.toCharArray(), s2.toCharArray(), 0);
+    }
+
+    public int kSimilarity(char[] a, char[] b, int startPoint) {
+        String t = new String(a);
+        if (mapit.get(t) != null) return mapit.get(t);
+        int index = startPoint;
+        while (index < a.length && a[index] == b[index]) {
+            index++;
+        }
+        if (index == a.length) return 0;
+        List<Integer> loc = new ArrayList<>();
+        char ch = b[index];
+        for (int start = index; start < a.length; start++) {
+            if (a[start] == ch) loc.add(start);
+        }
+        int min = Integer.MAX_VALUE;
+        char temp;
+        for (int swapIndex : loc) {
+            temp = a[swapIndex];
+            a[swapIndex] = a[index];
+            a[index] = temp;
+            min = Math.min(min, kSimilarity(a, b, index + 1));
+            temp = a[swapIndex];
+            a[swapIndex] = a[index];
+            a[index] = temp;
+        }
+        mapit.put(t, min + 1);
+        return min + 1;
+    }
+
+    //https://leetcode.com/problems/keys-and-rooms
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visited = new HashSet<>();
+        traverse(0, visited, rooms);
+        return visited.size() == rooms.size();
 
     }
+
+    public void traverse(int cur, Set<Integer> visited, List<List<Integer>> rooms) {
+        if (visited.contains(cur)) return;
+        visited.add(cur);
+        if (rooms.get(cur) == null || rooms.get(cur).size() <= 0) return;
+        for (int key : rooms.get(cur)) {
+            traverse(key, visited, rooms);
+        }
+    }
+
+    //https://leetcode.com/problems/group-anagrams
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> mapit = new HashMap<>();
+        for (String s : strs) {
+            char[] temp = s.toCharArray();
+            Arrays.sort(temp);
+            String addme = new String(temp);
+            List<String> addhere = mapit.getOrDefault(addme, new ArrayList<>());
+            addhere.add(s);
+            mapit.put(addme, addhere);
+        }
+        for (String key : mapit.keySet()) {
+            result.add(mapit.get(key));
+        }
+        return result;
+    }
+
+    //https://leetcode.com/problems/similar-string-groups
+    public int numSimilarGroups(String[] strs) {
+        int[] parent = new int[strs.length];
+        for (int y = 0; y < parent.length; y++) {
+            parent[y] = y;
+        }
+
+        for (int y = 0; y < strs.length - 1; y++) {
+            for (int j = y + 1; j < strs.length; j++) {
+                if (isSimilar(strs[y], strs[j])) {
+                    join(y, j, parent);
+                }
+            }
+        }
+        normalize(parent);
+        int count = 0;
+        for (int y = 0; y < parent.length; y++) {
+            if (parent[y] == y) count++;
+        }
+        return count;
+    }
+
+    public boolean isSimilar(String a, String b) {
+        int count = 0;
+        int index = 0;
+        while (index < a.length()) {
+            if (a.charAt(index) != b.charAt(index)) count++;
+            index++;
+            if (count > 2) return false;
+        }
+        return count == 2 || count == 0;
+    }
+
+    public void normalize(int[] parent) {
+        for (int y = 0; y < parent.length; y++) {
+            findParent(y, parent);
+        }
+    }
+
+    public void join(int a, int b, int[] parent) {
+        int pb = findParent(b, parent);
+        parent[pb] = findParent(a, parent);
+        return;
+    }
+
     //https://leetcode.com/problems/find-eventual-safe-states
     public List<Integer> eventualSafeNodes(int[][] graph) {
         // 0 unvisited 1 means safe 2 means loop
-        int visit[]=new int[graph.length];
-        Arrays.fill(visit,0);
-        for(int y=0;y<visit.length;y++){
-            if(visit[y]==0){
-                traverse(graph,visit,y,new HashSet<>());
+        int[] visit = new int[graph.length];
+        Arrays.fill(visit, 0);
+        for (int y = 0; y < visit.length; y++) {
+            if (visit[y] == 0) {
+                traverse(graph, visit, y, new HashSet<>());
             }
         }
         List<Integer> result=new ArrayList<>();
@@ -87,8 +187,8 @@ public class LeetCode1 {
     }
     //https://leetcode.com/problems/is-graph-bipartite/
     public boolean isBipartite(int[][] graph) {
-        int color[]=new int[graph.length];
-        int visitedCount=0;
+        int[] color = new int[graph.length];
+        int visitedCount = 0;
         Queue<Integer> q=new LinkedList<>();
         while(visitedCount<color.length){
             for(int y=0;y<color.length;y++){
@@ -124,7 +224,7 @@ public class LeetCode1 {
 
         if(children.get(k)==null)return -1;
 
-        int minCost[]=new int[n+1];
+        int[] minCost = new int[n + 1];
         minCost[0]=0;minCost[k]=0;
         for(int y=1;y<minCost.length;y++){
             if(y==k)continue;
@@ -138,11 +238,11 @@ public class LeetCode1 {
             }
         });
 
-        int currTime=0;
-        int v[]=new int[2];
+        int currTime = 0;
+        int[] v = new int[2];
         v[0]=k;v[1]=0;
         pq.add(v);
-        int parent[];
+        int[] parent;
         while(!pq.isEmpty()){
             parent=pq.poll();
             currTime=parent[1];
@@ -170,45 +270,47 @@ public class LeetCode1 {
     //parent child cost
 
     public void prepareList(Map<Integer,Map<Integer,Integer>> children,int [][]times,int k){
-        for(int t[]:times){
-            if(t[1]==k)continue;
-            Map<Integer,Integer> addme=children.getOrDefault(t[0],new HashMap<>());
-            addme.put(t[1],t[2]);
-            children.put(t[0],addme);
+        for (int[] t : times) {
+            if (t[1] == k) continue;
+            Map<Integer, Integer> addme = children.getOrDefault(t[0], new HashMap<>());
+            addme.put(t[1], t[2]);
+            children.put(t[0], addme);
         }
     }
     //https://leetcode.com/problems/redundant-connection
     public int[] findRedundantConnection(int[][] edges) {
-        int parent[]=new int[edges.length+1];
-        for(int y=0;y<parent.length;y++)parent[y]=y;
-        int  result[]=new int[2];
-        for(int a[]:edges){
-            if(findParent(a[0],parent)==findParent(a[1],parent)){
-                result[0]=a[0];
-                result[1]=a[1];
+        int[] parent = new int[edges.length + 1];
+        for (int y = 0; y < parent.length; y++) parent[y] = y;
+        int[] result = new int[2];
+        for (int[] a : edges) {
+            if (findParent(a[0], parent) == findParent(a[1], parent)) {
+                result[0] = a[0];
+                result[1] = a[1];
                 continue;
             }
-            merge(a[0],a[1],parent);
+            merge(a[0], a[1], parent);
         }
         return result;
     }
-    public int findParent(int a,int array[]){
-        if(a==array[a])return a;
-        int ab=findParent(array[a],array);
-        array[a]=ab;
+
+    public int findParent(int a, int[] array) {
+        if (a == array[a]) return a;
+        int ab = findParent(array[a], array);
+        array[a] = ab;
         return array[a];
     }
 
-    public void merge(int a,int b,int array[]){
-        int pa=findParent(a,array);
-        int pb=findParent(b,array);
-        array[pa]=array[pb];
+    public void merge(int a, int b, int[] array) {
+        int pa = findParent(a, array);
+        int pb = findParent(b, array);
+        array[pa] = array[pb];
     }
+
     //https://leetcode.com/problems/reconstruct-itinerary/
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String,List<String>> list= prepList(tickets);
-        List<String> result=new ArrayList<>();
-        traverse(list,"JFK",result);
+        Map<String, List<String>> list = prepList(tickets);
+        List<String> result = new ArrayList<>();
+        traverse(list, "JFK", result);
         return result;
     }
     public void traverse(Map<String,List<String>> adjList,String key,List<String> result){
@@ -277,15 +379,15 @@ public class LeetCode1 {
 
     public Map<Integer,Set<Integer>> prepareAdjList(int[][] edges){
         Map<Integer,Set<Integer>> adjList=new HashMap<>();
-        for(int a[]:edges){
+        for (int[] a : edges) {
 
-            Set<Integer> addme=adjList.getOrDefault(a[0],new HashSet<>());
+            Set<Integer> addme = adjList.getOrDefault(a[0], new HashSet<>());
             addme.add(a[1]);
-            adjList.put(a[0],addme);
+            adjList.put(a[0], addme);
 
-            addme=adjList.getOrDefault(a[1],new HashSet<>());
+            addme = adjList.getOrDefault(a[1], new HashSet<>());
             addme.add(a[0]);
-            adjList.put(a[1],addme);
+            adjList.put(a[1], addme);
         }
         return adjList;
     }
@@ -351,8 +453,8 @@ public class LeetCode1 {
 
     public Map<String,Integer> prepCost(int [][]flights){
         Map<String,Integer> cost=new HashMap<>();
-        for(int a[]:flights){
-            cost.put(("s:"+a[0]+":d:"+a[1]),a[2]);
+        for (int[] a : flights) {
+            cost.put(("s:" + a[0] + ":d:" + a[1]), a[2]);
         }
         return cost;
     }
@@ -368,26 +470,27 @@ public class LeetCode1 {
     }
 
     //https://leetcode.com/problems/critical-connections-in-a-network
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections){
-        List<List<Integer>> result=new ArrayList<>();
-        int min[]=new int[n];
-        int disc[]=new int[n];
-        Map<Integer,List<Integer>> adjList=prepareList(connections);
-        traverse(adjList,0,0,new HashSet<>(),result,min,disc);
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[] min = new int[n];
+        int[] disc = new int[n];
+        Map<Integer, List<Integer>> adjList = prepareList(connections);
+        traverse(adjList, 0, 0, new HashSet<>(), result, min, disc);
         return result;
     }
 
-    public int traverse(Map<Integer,List<Integer>> adjList,int v,int parent,Set<Integer> visited,List<List<Integer>> result,int min[],int disc[]){
-        if(visited.contains(v)){
+    public int traverse(Map<Integer, List<Integer>> adjList, int v, int parent, Set<Integer> visited, List<List<Integer>> result, int[] min, int[] disc) {
+        if (visited.contains(v)) {
             return min[v];
         }
         visited.add(v);
-        disc[v]=time;min[v]=time;
+        disc[v] = time;
+        min[v] = time;
         time++;
-        if(adjList.get(v)==null)return disc[v];
+        if (adjList.get(v) == null) return disc[v];
         //iterate all child node
-        for(Integer child:adjList.get(v)){
-            if(child!=parent){
+        for (Integer child : adjList.get(v)) {
+            if (child != parent) {
                 int childMin=traverse(adjList,child,v,visited,result,min,disc);
                 if(childMin>disc[v]){
                     List<Integer> addme=new ArrayList<>();
@@ -421,22 +524,23 @@ public class LeetCode1 {
         if(grid.length==1 && grid[0].length==1 && grid[0][0]==0)return 0;
         boolean [][]visited=new boolean[grid.length][grid[0].length];
         int island=0;
-        for(int r=0;r<grid.length;r++){
-            for(int c=0;c<grid[r].length;c++){
-                if(visited[r][c]==false && grid[r][c]=='1'){
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                if (visited[r][c] == false && grid[r][c] == '1') {
                     island++;
-                    traverse(r,c,grid,visited);
+                    traverse(r, c, grid, visited);
                 }
             }
         }
         return island;
     }
-    public void traverse(int r,int c,char grid[][],boolean visited[][]){
-        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || visited[r][c] || grid[r][c]=='0')return;
-        visited[r][c]=true;
-        traverse(r+1,c,grid,visited);
-        traverse(r-1,c,grid,visited);
-        traverse(r,c+1,grid,visited);
-        traverse(r,c-1,grid,visited);
+
+    public void traverse(int r, int c, char[][] grid, boolean[][] visited) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || visited[r][c] || grid[r][c] == '0') return;
+        visited[r][c] = true;
+        traverse(r + 1, c, grid, visited);
+        traverse(r - 1, c, grid, visited);
+        traverse(r, c + 1, grid, visited);
+        traverse(r, c - 1, grid, visited);
     }
 }
